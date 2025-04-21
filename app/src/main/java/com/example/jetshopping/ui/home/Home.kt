e07d84a464a07ad289c85dc9dfd65030a502e42f
Studggle <vg1992005@gmail.com>
2025-04-22 03:23:07 +0530

    Adding some terms in home.kt file.

--- app/src/main/java/com/example/jetshopping/ui/home/Home.kt
+++ app/src/main/java/com/example/jetshopping/ui/home/Home.kt
@@ -1,11 +1,95 @@
 package com.example.jetshopping.ui.home
 
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.material3.Card
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
 fun HomeScreen(
     onNavigateToItem: (Int) -> Unit,
 ) {
-    val homeViewModel = viewModel(modelClass = HomeViewModel::class.java))
+    val homeViewModel = viewModel(modelClass = HomeViewModel::class.java)
+    val homeState = homeViewModel.state
+    Scaffold(
+        floatingActionButton = {
+            FloatingActionButton(onClick = { onNavigateToItem(-1) }) {
+                Icon(imageVector = Icons.Default.Add, contentDescription = null)
+            }
+        }
+    ) {
+        LazyColumn {
+            item {
+                LazyRow {
+                    
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun CategoryItems(
+    @DrawableRes iconRes: Int,
+    title: String,
+    selected: Boolean,
+    onItemClick: () -> Unit,
+) {
+    Card(
+        modifier = Modifier
+            .padding(top = 8.dp, bottom = 8.dp, start = 8.dp)
+            .selectable(
+                selected = selected,
+                interactionSource = remember { MutableInteractionSource() },
+                indication = rememberRipple(),
+                onClick = onItemClick
+            ),
+        border = BorderStroke(
+            1.dp,
+            if (selected) MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+            else MaterialTheme.colorScheme.onSurface,
+        ),
+        shape = Shapes.large,
+        backgroundColor= if (selected) MaterialTheme.colorScheme.primary.copy(alpha = 0.5f)
+        else MaterialTheme.colorScheme.surface,
+        contentColor = if (selected) MaterialTheme.colorScheme.onPrimary
+        else MaterialTheme.colorScheme.onSurface,
+    ) {
+        // Add content here
+        Row(horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(8.dp)
+        ){
+            Icon(painter = painterResource(id = iconRes),
+                contentDescription = null),
+                modifier= Modifier.size(24.dp),
+            )
+        }
+    }
+}
+
+@Composable
+fun BorderStroke() {
+    TODO("Not yet implemented")
 }
