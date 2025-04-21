6f0c68ea8b1d602e0a3e686aa7273b06ed3bf826
Studggle <vg1992005@gmail.com>
2025-04-21 21:51:15 +0530

    Update HomeViewModel.kt

--- app/src/main/java/com/example/jetshopping/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/jetshopping/ui/home/HomeViewModel.kt
@@ -1,6 +1,5 @@
 package com.example.jetshopping.ui.home
 
-import android.icu.util.ULocale.Category
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -8,16 +7,32 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetshopping.Graph
 import com.example.jetshopping.data.room.ItemsWithStoreAndList
+import com.example.jetshopping.data.room.models.Item
 import com.example.jetshopping.ui.repository.Repository
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import java.util.Locale.Category
+
+enum class Category {
+    ALL,
+    GROCERY,
+    CLOTHING,
+    ELECTRONICS,
+    HOME,
+    OTHER
+}
 
 class HomeViewModel(
     private val repository: Repository=Graph.repository
 ) : ViewModel(){
     var state by mutableStateOf(HomeState())
         private set
+
+
+    init {
+        getItems()
+    }
+
+
     private fun getItems(){
         viewModelScope.launch {
             repository.getItemsWithListAndStore.collectLatest{
@@ -27,11 +42,44 @@ class HomeViewModel(
             }
         }
     }
+
+    fun deleteItem(item: Item){
+        viewModelScope.launch {
+            repository.deleteItem(item)
+        }
+    }
+    fun onCategoryChange(category: Category){
+        state = state.copy(category = category)
+        filterItems(10001)
+    }
+
+
+
+
+     private fun filterItems(shoppingListId:Int){
+         if (shoppingListId != 10001) {
+             viewModelScope.launch {
+                 repository.getItemWithStoreAndListFilteredById(
+                     shoppingListId
+                 ).collectLatest {
+                     state = state.copy(
+                         items = it
+                     )
+                 }
+             }
+         }else{
+             getItems()
+         }
+     }
+
+
+
+
+    
 }
 
 data class HomeState(
     val items: List<ItemsWithStoreAndList> = emptyList(),
-    val category: Category = Category(),
+    val category: Category = Category.ALL,
     val itemChecked: Boolean = false
-
 )
